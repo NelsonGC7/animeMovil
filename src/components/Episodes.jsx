4d463701515257e5707c1546b55c497e60f88936
PittnoiR <omargarza394@gmail.com>
2024-09-21 11:04:23 -0700

    +estilos de capitulos

---
+++ src/components/Episodes.jsx
@@ -0,0 +1,40 @@
+import '../styles/episodes.css'
+
+
+export const Episodes = ({name,cap}) => {
+    return(
+        <aside className="active">
+        <h2>naruto</h2>
+        <div className="episodes" > 
+            <div className="episodes_box">
+                <p>1</p>
+            </div>
+            <div className="episodes_box">
+                <p>2</p>
+            </div>
+            <div className="episodes_box">
+                <p>3</p>
+            </div>
+            <div className="episodes_box">
+                <p>4</p>
+            </div>
+            <div className="episodes_box">
+                <p>5</p>
+            </div>
+            <div className="episodes_box">
+                <p>6</p>
+            </div>
+            <div className="episodes_box">
+                <p>7</p>
+            </div>
+            
+
+        </div>
+        
+        
+        </aside>
+
+    )
+
+
+}
