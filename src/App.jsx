99cb52c516b64a086c3cd7c9c44682696c846ab4
PittnoiR <omargarza394@gmail.com>
2024-09-22 10:07:33 -0700

    grid de capitulos creado

--- src/App.jsx
+++ src/App.jsx
@@ -1,85 +1,3 @@
-
-/*
-  import { useState, useEffect } from 'react';
-import './styles/App.css';
-import { Topanime } from './components/top.jsx';
-
-function App() {
-  const [recentAnimes, setRecentAnimes] = useState([]);
-  const [previousMonth, setPreviousMonth] = useState(false); // Estado para manejar el mes anterior
-
-  const getRecentAnimes = (getPreviousMonth = false) => {
-    const today = new Date();
-    const endDate = today.toISOString().split('T')[0];
-
-    const startDate = new Date(today);
-
-    if (getPreviousMonth) {
-      startDate.setMonth(today.getMonth() - 2);
-    } else {
-      // Si es el mes actual, restamos solo 1 mes
-      startDate.setMonth(today.getMonth() - 1);
-    }
-
-    const startDateFormatted = startDate.toISOString().split('T')[0];
-
-
-
-*/
-
-/*
-    fetch(requestUrl)
-      .then(response => response.json())
-      .then(data => {
-        console.log("Animes Data:", data.data);
-        setRecentAnimes(data.data); // Actualiza el estado con los datos recibidos
-      })
-      .catch(error => {
-        console.error('Error al obtener los animes:', error);
-      });
-  };
-
-  useEffect(() => {
-    getRecentAnimes(previousMonth);
-  }, [previousMonth]);
-
-  const handlePreviousMonthClick = () => {
-    setPreviousMonth(true); // Cambia el estado para obtener datos del mes anterior
-  };
-    
-    */
-/*
-      <header className="section_compani">
-        <h1>AnimeTotal</h1>
-        <p>new</p>
-      </header>
-
-      <section className="container">
-        <div className="column">
-          {recentAnimes.map((anime, index) => (
-            <div key={index} className="producto">
-              <p className='fecha'>
-                <small>Estreno: </small>
-                {anime.aired?.from ? new Date(anime.aired.from).toLocaleDateString() : "Fecha no disponible"}
-              </p>
-              <br />
-              {anime.images?.jpg?.large_image_url && (
-                <img src={anime.images.jpg.large_image_url} alt={anime.title} />
-              )}
-              <p>{anime.title}</p>
-              <br/>
-              <small>Episodios: {anime.episodes}</small>
-            </div>
-          ))}
-        </div>
-      </section>
-
-    
-      <button onClick={handlePreviousMonthClick}>Mes antes</button>
-
-      
-      <Topanime />
-       */
 import { Header } from "./components/Header";
 import { Media } from "./components/Media";
 import { Informacion } from "./components/Informacion";
@@ -106,10 +24,10 @@ function App() {
 
    async function llamada() {
     const res =  await fetch(`https://api.jikan.moe/v4/anime?q=${title}`)
-    const data =  await res.json()
-    setAnime(data.data[0])
-    setUrlimg(data.data[0].images.jpg.image_url)
-    setGeneros(data.data[0].genres)
+    const data =  await res.json();
+    setAnime(data.data[0]);
+    setUrlimg(data.data[0].images.jpg.image_url);
+    setGeneros(data.data[0].genres);
    }
   useEffect(()=>{
    llamada()
--- src/components/Episodes.jsx
+++ src/components/Episodes.jsx
@@ -4,8 +4,113 @@ import '../styles/episodes.css'
 export const Episodes = ({name,cap}) => {
     return(
         <aside className="active">
-        <h2>naruto</h2>
+        <h2>SAO</h2>
         <div className="episodes" > 
+            <div className="episodes_box">
+                <p>1</p>
+            </div>
+            <div className="episodes_box">
+                <p>2</p>
+            </div>
+            <div className="episodes_box">
+                <p>3</p>
+            </div>
+            <div className="episodes_box">
+                <p>4</p>
+            </div>
+            <div className="episodes_box">
+                <p>5</p>
+            </div>
+            <div className="episodes_box">
+                <p>6</p>
+            </div>
+            <div className="episodes_box">
+                <p>7</p>
+            </div>
+            <div className="episodes_box">
+                <p>1</p>
+            </div>
+            <div className="episodes_box">
+                <p>2</p>
+            </div>
+            <div className="episodes_box">
+                <p>3</p>
+            </div>
+            <div className="episodes_box">
+                <p>4</p>
+            </div>
+            <div className="episodes_box">
+                <p>5</p>
+            </div>
+            <div className="episodes_box">
+                <p>6</p>
+            </div>
+            <div className="episodes_box">
+                <p>7</p>
+            </div>
+            <div className="episodes_box">
+                <p>1</p>
+            </div>
+            <div className="episodes_box">
+                <p>2</p>
+            </div>
+            <div className="episodes_box">
+                <p>3</p>
+            </div>
+            <div className="episodes_box">
+                <p>4</p>
+            </div>
+            <div className="episodes_box">
+                <p>5</p>
+            </div>
+            <div className="episodes_box">
+                <p>6</p>
+            </div>
+            <div className="episodes_box">
+                <p>7</p>
+            </div>
+            <div className="episodes_box">
+                <p>1</p>
+            </div>
+            <div className="episodes_box">
+                <p>2</p>
+            </div>
+            <div className="episodes_box">
+                <p>3</p>
+            </div>
+            <div className="episodes_box">
+                <p>4</p>
+            </div>
+            <div className="episodes_box">
+                <p>5</p>
+            </div>
+            <div className="episodes_box">
+                <p>6</p>
+            </div>
+            <div className="episodes_box">
+                <p>7</p>
+            </div>
+            <div className="episodes_box">
+                <p>1</p>
+            </div>
+            <div className="episodes_box">
+                <p>2</p>
+            </div>
+            <div className="episodes_box">
+                <p>3</p>
+            </div>
+            <div className="episodes_box">
+                <p>4</p>
+            </div>
+            <div className="episodes_box">
+                <p>5</p>
+            </div>
+            <div className="episodes_box">
+                <p>6</p>
+            </div>
+            <div className="episodes_box">
+                <p>7</p>
+            </div>
             <div className="episodes_box">
                 <p>1</p>
             </div>
